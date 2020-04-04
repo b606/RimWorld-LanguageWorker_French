80285f8d24154a7924a2856c1099f059ed8fa0a2
b606 <besoa606@gmail.com>
2020-04-04 15:31:07 +0200

    changed assembly Guid (and incidentally other projects Guid)

--- RimWorld_LanguageWorker_French/Properties/AssemblyInfo.cs
+++ RimWorld_LanguageWorker_French/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
+[assembly: Guid("021fcf6a-89ef-489d-9596-f7948e35f392")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.15.1.0")]
-[assembly: AssemblyFileVersion("0.15.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
